cf39ee3e8f98291273b921f85f6cda2464fb0716
Isfhan Ahmed <isfhan729@gmail.com>
2020-06-05 01:39:54 +0500

    modifay some data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hi, Isfhan Ahmed is here
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://isfhan.surge.sh"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My web CV 
         </a>
       </header>
     </div>
